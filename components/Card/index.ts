ba51530499dd1cd48e50abe177ae360e9ebc05b3
benlittlenz <benlittle53@gmail.com>
2021-12-30 11:50:26 +1300

    Feat: Added card component

---
+++ components/Card/index.ts
@@ -0,0 +1,82 @@
+import styled from "styled-components"
+import { ShoppingCartIcon } from "@heroicons/react/outline";
+
+export const ImageContainer = styled.div`
+  position: relative;
+  width: 100%;
+  min-height: 20rem;
+  border-radius: 0.375rem;
+  overflow: hidden;
+
+  @media (min-width: 1024px) {
+    height: 20rem;
+  }
+
+  // shadow 162533
+`;
+
+export const CardInfoWrapper = styled.div`
+
+`;
+
+export const CardInfoHeader = styled.h3`
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  color: #374151;
+  cursor: pointer;
+`;
+
+export const CardInfoDescription = styled.p`
+  margin-top: 4px;
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  color: #6b7280;
+`;
+
+export const CardPriceWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  visibility: hidden;
+
+  span {
+    font-size: 1.2rem;
+    line-height: 1.25rem;
+    color: #374151;
+    font-weight: 800;
+  }
+`;
+
+export const CardCheckout = styled(ShoppingCartIcon)`
+  width: 20px;
+  height: 20px;
+  padding: 6px;
+  font-weight: 700;
+  color: white;
+  cursor: pointer;
+  background-color: black;
+  border-radius: 4px;
+`;
+
+export const CardWrapper = styled.div`
+  padding: 2rem 1.5rem;
+  overflow: hidden;
+  width: 280px;
+  max-width: 42rem;
+  border-radius: 5px;
+
+  &:hover {
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  }
+
+  &:hover {
+    ${CardPriceWrapper} {
+      visibility: visible;
+      margin-top: 20px;
+    }
+  }
+
+  @media (min-width: 1024px) {
+    max-width: 80rem;
+  }
+`;
--- components/FeaturedProducts/index.tsx
+++ components/FeaturedProducts/index.tsx
@@ -1,19 +1,103 @@
+import Image from "next/image";
 import styled from "styled-components";
+import {
+  CardWrapper,
+  ImageContainer,
+  CardInfoWrapper,
+  CardInfoHeader,
+  CardInfoDescription,
+  CardPriceWrapper,
+  CardCheckout,
+} from "../Card";
+
+const featuredList = [
+  {
+    image: "/painting.png",
+    title: "Wallowing Breeze",
+    description:
+      "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    price: "$500.00",
+  },
+  {
+    image: "/painting 2.png",
+    title: "J Resistance",
+    description:
+      "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    price: "$450.00",
+  },
+  {
+    image: "/painting 4.png",
+    title: "Warm Basket",
+    description:
+      "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    price: "$420.00",
+  },
+  {
+    image: "/painting 2.png",
+    title: "The Vonnergut",
+    description:
+      "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    price: "$380.00",
+  },
+];
 
 export function FeaturedProducts() {
   return (
-    <ProductsWrapper>
-      <Heading>
-        Featured
-      </Heading>
-    </ProductsWrapper>
+    <Wrapper>
+      <Heading>Featured</Heading>
+      <ProductsWrapper>
+        {featuredList.map((feature) => (
+          <CardWrapper>
+            <ImageContainer>
+              <Image
+                src={feature.image}
+                layout="fixed"
+                width={300}
+                height={300}
+              />
+            </ImageContainer>
+            <CardInfoWrapper>
+              <CardInfoHeader>{feature.title}</CardInfoHeader>
+              <CardInfoDescription>{feature.description}</CardInfoDescription>
+              <CardPriceWrapper>
+                <span>{feature.price}</span>
+                <CardCheckout />
+              </CardPriceWrapper>
+            </CardInfoWrapper>
+          </CardWrapper>
+        ))}
+      </ProductsWrapper>
+    </Wrapper>
   );
 }
 
-const ProductsWrapper = styled.div`
-  background-color: white;
-  max-width: 42rem;
+const Wrapper = styled.div`
+  max-width: 80rem;
+  margin: 0 auto;
+`;
 
+const ProductsWrapper = styled.div`
+  display: grid;
+  margin-top: 1.5rem;
+  grid-template-columns: repeat(1, minmax(0, 1fr));
+  column-gap: 5rem;
+  row-gap: 2.5rem;
+
+  @media (min-width: 640px) {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+
+  @media (min-width: 1024px) {
+    grid-template-columns: repeat(4, minmax(0, 1fr));
+  }
+  @media (min-width: 1280px) {
+    column-gap: 2rem;
+  }
+  background-color: white;
+
+  @media (max-width: 768px) {
+    max-width: 42rem;
+  }
 `;
 
 const Heading = styled.h1`
